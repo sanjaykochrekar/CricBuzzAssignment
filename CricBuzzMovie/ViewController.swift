072c31b28e37f435d70ff6a088cd0f034dc6343b
sanjay <kochrekarsanjay87@gmail.com>
2023-09-30 12:26:24 +0530

    initial setup with pod

---
+++ CricBuzzMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CricBuzzMovie
+//
+//  Created by Sanju on 30/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
